7a33b62070865e050ff932ec7a7b6e7692e29ea8
Dipayan Dutta <inbox.dipayan@gmail.com>
2020-06-18 13:23:36 +0530

    User Profile Model Created

--- src/improveo/urls.py
+++ src/improveo/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('profiles.urls')),
 ]
 
+admin.site.site_header = 'Improveo Admin'
+admin.site.site_title  = 'Improveo Admin'
+admin.site.index_title = 'Improveo Code'
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio  = models.TextField()
+    profile_picture = models.ImageField(blank=True)
+    website = models.CharField(max_length=200)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    # String method to return the object 
+    def __str__(self):
+        return "{0} - {1}".format(self.user.first_name,self.user.last_name)
