b56b74c07089d92c51c9398e459f6ec7ca491ecc
Dipayan Dutta <inbox.dipayan@gmail.com>
2020-06-29 11:51:01 +0530

    some more line added

--- src/reports/views.py
+++ src/reports/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .forms import ReportForm,ProblemReportedForm
+from .models import Report
 
 # Create your views here.
 def report_view(request):
     form = ReportForm()
     pform = ProblemReportedForm()
-
-
+    #querySet = Report.objects.filter(production_line__name=production_line)
+    querySet = Report.objects.all()
     context = {
         'form':form,
-        'pform':pform
+        'pform':pform,
+        'object_list':querySet,
     }
 
     return render(request,'reports/report.html',context)
